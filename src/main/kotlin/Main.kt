16f70c7cb4ef084df0af1e61be4459c9884746ea
FunkenFlake <kustsoul@gmail.com>
2023-02-08 16:39:23 +0300

    Learn if\else

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,10 +11,41 @@ fun main() {
     println("The hero announces her presence to the world.")
     println(HERO_NAME)
 
-    var playerLevel = 5
+    var playerLevel = 4
     println(playerLevel)
 
-    println("The hero embarks on her journey to locate the enchanted sword.")
+    val hasAngeredBarbarians = false
+    val hasBefriendedBarbarians = true
+    val playerClass = "paladin"
+
+//    println("The hero embarks on her journey to locate the enchanted sword.")
+
+    val quest: String = if (playerLevel == 1) {
+        "Meet Mr. Bubbles in the land of soft things."
+    } else if (playerLevel <= 5) {
+        // ѕроверим возможность дипломатического решени€
+        val canTalkToBarbarians = !hasAngeredBarbarians &&
+                (hasBefriendedBarbarians || playerClass == "barbarian")
+        if (canTalkToBarbarians) {
+            "Convince the barbarians to call of their invasion."
+        } else {
+            "Save the town from the barbarian invasion."
+        }
+    } else if (playerLevel == 6) {
+        "Locate the enchanted sword."
+    } else if (playerLevel == 7) {
+        "Recover the long-lost artifact of creation."
+    } else if (playerLevel == 8) {
+        "Defeat Nogartse, bringer of death and eater of worlds."
+    } else {
+        "There are no quests right now."
+    }
+
+    println("The hero approaches the bounty board. It reads:")
+    println(quest)
+
+    println("Time passes...")
+    println("The hero returns from her quest.")
 
     playerLevel++
     println(playerLevel)
