d45338ee59289383a311e1163393f827d905a5e2
FunkenFlake <kustsoul@gmail.com>
2023-03-10 16:45:02 +0300

    Chapter 16 keep homework.

--- NeytHack/src/main/kotlin/chapter16/homework/NyetHack.kt
+++ NeytHack/src/main/kotlin/chapter16/homework/NyetHack.kt
@@ -1,8 +1,8 @@
 // начина€ с главы 13 классы, буду делать дл€ каждой главы директорию
 
-package chapter16
+package chapter16.homework
 
-import arrow.core.valid
+import chapter16.homework.Game.miniMap
 
 // определение класса создает соответствующий тип, поэтому
 // переменна€ player имеет тип Player
@@ -57,8 +57,16 @@ object Game {   // объект, то же самое, что и класс, только в 1 экземпл€ре
         listOf(Room("The Dangeon"))
     )
 
+    private val miniMap = mutableListOf(
+        listOf("[1:1]", "[1:2]", "[1:3]"),
+        listOf("[2:1]", "[2:2]"),
+        listOf("[3:1]")
+    )
+
     private var currentRoom: Room = worldMap[0][0] // имеет тип Room, так можно тк TownSquare подкласс Room
     private var currentPosition = Coordinate(0, 0)
+
+    private var playGame = true
 
     init {      // можно объ€вить инициализацию дл€ выполнени€, какого нить кода при запуске
         narrate("Welcome, adventurer")
@@ -67,7 +75,7 @@ object Game {   // объект, то же самое, что и класс, только в 1 экземпл€ре
     }
 
     fun play() {// чтобы запустить объект нужно вызвать, что-то из него, вызовем эту ф-ию
-        while (true) {
+        while (playGame) {
             // Game
             narrate("${player.name} of ${player.hometown}, ${player.title}," +
                     " is in ${currentRoom.description()}")
@@ -106,6 +114,44 @@ object Game {   // объект, то же самое, что и класс, только в 1 экземпл€ре
                     narrate("I don't know what direction that is")
                 }
             }
+
+            "cast" -> {
+                // хотел добавить еще frostball , но что-то не получилось
+                val spell = SpellBook.values()
+                    .firstOrNull { it.name.equals(argument, ignoreCase = true) }
+
+                if (spell != null) {
+                    player.castFireball()
+                } else {
+                    narrate("I don't know what spell that is")
+                }
+            }
+
+            "prophesize" -> player.prophesize()
+
+            "map" -> {
+                miniMap[0][1]
+                println(miniMap[currentPosition.x][currentPosition.y])
+
+                miniMap.forEachIndexed { index, it ->
+                    if (currentPosition.y == index) {
+                        println("$index, $it")
+                        it.forEachIndexed { index, it ->
+                            if (currentPosition.x == index) {
+                                println("$index, $it")
+//                                miniMap.add(index, "jj")
+                            }
+                        }
+                    }
+//                    println("$index, $it")
+
+                }
+            }
+
+            "ring" -> narrate("игрок бьет в колокол люобе кол-во раз")
+
+            "exit" -> playGame = false
+
             else -> narrate("I'm not sure what you're trying to do")
         }
     }
