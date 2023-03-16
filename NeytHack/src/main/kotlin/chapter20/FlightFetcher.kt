94d91d089c375a909f697c085d30c5cd74af7a42
FunkenFlake <kustsoul@gmail.com>
2023-03-16 16:27:52 +0300

    Chapter 20 begin coroutines yaaay.

---
+++ NeytHack/src/main/kotlin/chapter20/FlightFetcher.kt
@@ -0,0 +1,22 @@
+package chapter20
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.net.URL
+
+private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
+private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
+
+fun main() {
+    runBlocking {
+        println("Started")
+        launch {
+            val flight = fetchFlight()
+            println(flight)
+        }
+        println("Finished")
+    }
+}
+
+fun fetchFlight(): String = URL(FLIGHT_ENDPOINT).readText()
