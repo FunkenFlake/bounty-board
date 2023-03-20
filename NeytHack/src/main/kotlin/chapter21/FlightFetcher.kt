0c5b5b925e0781565d00ee72c32687af9d72953f
FunkenFlake <kustsoul@gmail.com>
2023-03-20 11:27:17 +0300

    Chapter 21 begin.

---
+++ NeytHack/src/main/kotlin/chapter21/FlightFetcher.kt
@@ -0,0 +1,54 @@
+package chapter21
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.request.get
+import kotlinx.coroutines.*
+import java.net.URL
+
+private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
+private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
+private const val LOYALTY_ENDPOINT = "$BASE_URL/loyalty"
+
+fun main() {
+    println("with out coroutine")
+    runBlocking {
+        println("Started")
+        launch {
+            val flight = fetchFlight("Madrigal")
+            println(flight)
+        }
+        println("Finished")
+    }
+
+    println("end")
+}
+
+suspend fun fetchFlight(passengerName: String): FlightStatus = coroutineScope {
+    val client = HttpClient(CIO)
+
+
+    val flightResponse = async {
+        println("Started fetching flight info")
+        client.get<String>(FLIGHT_ENDPOINT).also {
+            println("Finished fetching flight info")
+        }
+    }
+
+    println("Started fetching loyalty info")
+    val loyaltyResponse = async {
+        client.get<String>(LOYALTY_ENDPOINT).also {
+            println("Finished fetching loyalty info")
+        }
+    }
+
+    delay(500)
+    println("Combining flight data")
+
+//    return "$flightResponse\n$loyaltyResponse"
+    FlightStatus.parse(
+        passengerName = passengerName,
+        flightResponse = flightResponse.await(),
+        loyaltyResponse = loyaltyResponse.await()
+    )
+}
