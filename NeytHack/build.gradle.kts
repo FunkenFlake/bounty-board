39bfa368c74d0927e30a714f4f81c674aa68cedd
FunkenFlake <kustsoul@gmail.com>
2023-03-20 11:16:03 +0300

    Chapter 20 done without homework.

--- NeytHack/build.gradle.kts
+++ NeytHack/build.gradle.kts
@@ -1,9 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.20.0")
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.7.20"
     application
 }
+
+apply(plugin = "kotlinx-atomicfu")
 
 group = "me.user"
 version = "1.0-SNAPSHOT"
