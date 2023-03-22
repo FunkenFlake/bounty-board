09370f0c2f4c4bb63f7d48c20b0275832d429be8
FunkenFlake <kustsoul@gmail.com>
2023-03-22 09:17:42 +0300

    Chapter 23 begin.

---
+++ NeytHack/src/main/java/Jhava.java
@@ -0,0 +1,11 @@
+// for chapter23 (main/kotlin/chapter23)
+
+public class Jhava {
+    public String utterGreeting() {
+        return "BLARGH";
+    }
+
+    public String determineFriendshipLevel() {
+        return null;
+    }
+}
---
+++ NeytHack/src/main/kotlin/chapter23/Hero.kt
@@ -0,0 +1,10 @@
+// for Jhava (main/java/Jhava)
+
+package chapter23
+
+import Jhava
+
+fun main() {
+    val adversary = Jhava()
+    println(adversary.utterGreeting())
+}
